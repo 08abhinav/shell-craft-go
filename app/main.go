76f9bc958e604122e228260cc17216bd3e0b1daa
08abhinav <abhinavnegi101@gmail.com>
2026-09-07 12:14:40 +0530

    update: formatted the code

--- app/main.go
+++ app/main.go
@@ -5,14 +5,13 @@ import (
 	"os"
 	"fmt"
 	"strings"
+	"github.com/codecrafters-io/shell-starter-go/app/utils"
 )
 
 // Ensures gofmt doesn't remove the "fmt" import in stage 1 (feel free to remove this!)
 var _ = fmt.Print
 
 func main() {
-	// TODO: Uncomment the code below to pass the first stage
-	builtinCommands := []string{"echo", "type", "exit"}
 	for{
 
 		fmt.Print("$ ")
@@ -30,17 +29,17 @@ func main() {
 		}else if strings.HasPrefix(command, "echo "){
 			fmt.Println(command[5: ])
 		}else if strings.HasPrefix(command, "type "){
-			found := false;
-			for _, i := range builtinCommands{
-				if command[5: ] == i{
-					fmt.Println(command[5: ], "is a shell builtin")
-					found = true
-					break
-				}
+			token := command[5:]
+
+			path, isBuiltin, err := utils.FindCommand(token)
+			if isBuiltin {
+				fmt.Printf("%s is a shell builtin\n", token)
+			} else if err == nil {
+				fmt.Printf("%s is %s\n", token, path)
+			} else {
+				fmt.Printf("%s: not found\n", token)
 			}
-			if !found {
-				fmt.Println( command[5: ], ": not found")
-			}
+
 		}else{
 			fmt.Println(command + ": command not found")
 		}
