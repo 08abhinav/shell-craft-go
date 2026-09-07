8b2cde68b9d3d89e42005545c78e5340565efc22
08abhinav <abhinavnegi101@gmail.com>
2026-09-07 15:49:25 +0530

    feat: implemented builtin helper for pwd and cd commands

--- app/utils/helpLocater.go
+++ app/utils/helpLocater.go
@@ -2,6 +2,7 @@ package utils
 
 import (
 	"errors"
+	"os"
 	"os/exec"
 	"strings"
 )	
@@ -44,4 +45,37 @@ func RunProgram(input string) (string, error){
 	}
 
 	return "", ErrNotFound
+}
+
+func BuiltinExecute(cmd string) (string, bool, error) {
+	value := strings.Fields(cmd)
+	if len(value) == 0 {
+		return "", false, nil
+	}
+
+	command := value[0]
+	args := value[1:]
+
+	switch command {
+	case "pwd":
+		dir, err := os.Getwd()
+		if err != nil {
+			return "", true, err
+		}
+		return dir, true, nil
+
+	case "cd":
+		if len(args) != 1{
+			return "", true, ErrNotFound
+		}
+
+		err := os.Chdir(args[0])
+		if err != nil {
+			return "", true, err
+		}
+		return "", true, nil
+
+	default:
+		return "", false, nil
+	}
 }
